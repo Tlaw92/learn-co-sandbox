af11144ccea950b43a5de87dad851ae7e8bf4bec
travis Lawson <travislawclimb@gmail.com>
2020-05-07 08:15:55 +0000

    Done.

---
+++ case.rb
@@ -0,0 +1,8 @@
+case greeting
+  when "unfriendly_greeting"
+    puts "What do you want!?"
+  when "friendly_greeting"
+    puts "Hi! How are you?
+  end  
+end  
+
---
+++ notes.rb
@@ -0,0 +1,9 @@
+brothers = ["Tom", "Tim", "Jim"]
+
+count = 0
+while count <= brothers.length-1
+  
+  puts "Stop hitting yourself #{brothers[count]}!"
+  
+  count += 1
+end
